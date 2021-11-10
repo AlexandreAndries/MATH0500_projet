6042f03a6a03f42db0b28684221eb2cff9787c31
AlexandreAndries <alexandreandries23@gmail.com>
2021-11-10 16:24:15 +0100

    Création main

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
-// j'aime le caca
-int int main(int argc, char const *argv[]) {
-  int i;
-  return 0;
-}
+/**
+ * \file main.c
+ *
+ *
+ * \brief MATH0500 - Projet
+ * \author Andries Alexandre s196948
+ * \author Khaliphi Abdelilah s204896
+ * \version 0.1
+ * \date 10/11/2021
+ *
+ * Ce fichier contient le programmme principal (main)
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <assert.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <ctype.h>
+ #include <getopt.h>
+
+ /*----------------------------------------------------------------------------*/
+ /*---------------------------------MAIN PROGRAM-------------------------------*/
+ /*----------------------------------------------------------------------------*/
+int main(int argc, char *argv[]){
+  
+} //fin main()
+ /*----------------------------------------------------------------------------*/
+ /*----------------------------------FIN DU MAIN-------------------------------*/
+ /*----------------------------------------------------------------------------*/
