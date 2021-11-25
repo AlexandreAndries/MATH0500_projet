e1316798af53f40440d970d0e81d03431ba56cb9
AlexandreAndries <alexandreandries23@gmail.com>
2021-11-25 23:48:33 +0100

    Modifs fichiers operations.h/.c (rien d 'important)

---
+++ operations.c
@@ -0,0 +1,35 @@
+/**
+ * \file operations.h
+ *
+ *
+ * \brief MATH0500 - Projet
+ * \author Andries Alexandre s196948
+ * \author Khaliphi Abdelilah s204896
+ * \version 0.1
+ * \date 10/11/2021
+ *
+ * Ce fichier contient les les définitions des fonctions utilisées
+ * dans la gestion des calculs et opérations sur une matrice creuse,
+ * entre deux matrices creuses, entre une matrice creuse et
+ * un vecteur creux (resp. un vecteur dense).
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "array.h"
+#include "sparse-matrix.h"
+#include "operations.h"
+
+/*----------------------------------------------------------------------------*/
+/*------------------------------FONCTIONS STATIQUES---------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------FONCTIONS & PROCEDURES--------------------------*/
+/*----------------------------------------------------------------------------*/
+// FONCTION POUR TRANSPOSER MATRICE A FAIRE EN PRIORITE ULTIME !!!
+/*----------------------------------------------------------------------------*/
+/*--------------------------------FIN DU MODULE-------------------------------*/
+/*----------------------------------------------------------------------------*/
---
+++ operations.h
@@ -0,0 +1,40 @@
+/**
+ * \file operations.h
+ *
+ *
+ * \brief MATH0500 - Projet
+ * \author Andries Alexandre s196948
+ * \author Khaliphi Abdelilah s204896
+ * \version 0.1
+ * \date 10/11/2021
+ *
+ * Ce fichier contient les déclarations de types et les prototypes des
+ * fonctions utilisées dans la gestion des calculs et opérations sur une
+ * matrice creuse, entre deux matrices creuses, entre une matrice creuse et
+ * un vecteur creux (resp. un vecteur dense).
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "array.h"
+#include "sparse-matrix.h"
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------------INCLUDE GUARD-------------------------------*/
+/*----------------------------------------------------------------------------*/
+#ifndef __OPERATIONS__
+#define __OPERATIONS__
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------MACROS-----------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------FONCTIONS & PROCEDURES--------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------------FIN DU HEADER-------------------------------*/
+/*----------------------------------------------------------------------------*/
+#endif // __OPERATIONS__
